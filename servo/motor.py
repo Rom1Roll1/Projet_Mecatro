8183304fa0f5a9f94b0332563d601ce998535f5c
skron85 <sam.kron85@gmail.com>
2018-09-13 12:37:26 +0200

    Suite fonction déplacement moteur

--- servo/motor.py
+++ servo/motor.py
@@ -35,18 +35,18 @@ class Moteurs(object):
         self.Moteurs=myActuators
         return self
     
-    def conversion_angle_interval ( theta ):
-    """ Take an angle and convert it into a cervomotor interval """
+    def conversion_angle_interval( theta ):
+        """ Take an angle and convert it into a cervomotor interval """
         minimum_angle = 300 / 1024 #equivalent in ° of a motor interval
-        targeted_interval = theta / minimum_angle #equivalent of the targeted angle in motor interval
+        targeted_interval = round (theta / minimum_angle) #interger equivalent of the targeted angle in motor interval
         return targeted_interval
         
         
         
-    def move_motor (self, ID, theta_f, V = 255):
-    """ Take a motor ID, an ordered angle and the motor speed and make it move """
-        self.Moteurs.goal_position[ID] = 
-    
+    def move_motor(self, ID, theta_f, V = 255):
+        """ Take a motor ID, an ordered angle and the motor speed and make it move """
+        self.Moteurs.goal_position[ID] = conversion_angle_interval( theta_f )
+        residual_error = self.Moteurs.current_position - conversion_angle_interval (theta_f)
     
     def close(self):
         self.serial.close()
